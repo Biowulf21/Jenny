95eca54ce3b3c16d4e7bc2d5cf03349da9e45c0b
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2022-10-05 07:58:40 +0800

    Setup for Register Admin feature

---
+++ app/Http/Repositories/Admin/AdminRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Repositories\Admin;
+
+use App\Models\User;
+
+class AdminRepository implements AdminRepositoryInterface
+{
+
+   
+}
---
+++ app/Http/Repositories/Admin/AdminRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Repositories\Admin;
+
+use Illuminate\Http\Request;
+
+interface AdminRepositoryInterface {
+    // functions here
+}
