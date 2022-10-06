df203dcf18edd312a575f8eb4669953f07b928a2
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2022-10-06 13:36:07 +0800

    Created Create Question feature

--- app/Http/Repositories/Question/QuestionRepository.php
+++ app/Http/Repositories/Question/QuestionRepository.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Repositories\Question;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+use App\Exceptions\ValidatorFailedException;
+use App\Models\Question;
 
 class QuestionRepository implements QuestionRepositoryInterface
 {
+    public function createQuestion(array $data): Question
+    {
+        $validator = Validator::make($data, 
+            [
+                'exam_id' => 'required', 
+                'type' => 'required|in:radio,single,paragraph',
+                'problem' => 'required',
+                'options' => 'required|array',
+                'answer' => 'required',
+            ]
+        );
 
+        if($validator->fails())
+        {
+            throw new ValidatorFailedException('Failed creating exam', $validator->errors());
+        }
+
+        $validated = $validator->validated();
+
+        return Question::create($validated);
+
+    }
 
 }
--- app/Http/Repositories/Question/QuestionRepositoryInterface.php
+++ app/Http/Repositories/Question/QuestionRepositoryInterface.php
@@ -4,7 +4,9 @@ namespace App\Http\Repositories\Question;
 
 use Illuminate\Http\Request;
 
+use App\Models\Question;
+
 interface QuestionRepositoryInterface 
 {
-    
+    public function createQuestion(array $data): Question;
 }
