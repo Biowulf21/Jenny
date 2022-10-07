8855e3eb5708ac2d0846b5a6cf5555d2211cd112
James Jilhaney <notail2111@gmail.com>
2022-10-07 14:13:14 +0800

    Changed default password in userfactory and added random hash

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -17,7 +18,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('Password21!'), // password
             'role' => $this->faker->randomElement(['admin', 'applicant']),
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
