f783840be6ccdbdeec48a93b8790c027eeacb02e
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2022-10-07 08:33:42 +0800

    Further edited validation rules, and added code for custom error message

--- app/Http/Repositories/Question/QuestionRepository.php
+++ app/Http/Repositories/Question/QuestionRepository.php
@@ -13,30 +13,34 @@ class QuestionRepository implements QuestionRepositoryInterface
 {
     public function createQuestion(array $data): Question
     {
-        $validator = Validator::make($data, 
-            [
-                'exam_id' => 'required', 
-                'type' => 'required|in:radio,single,paragraph',
-                'problem' => 'required',
-                'options' => 'nullable|array|required_if:type,radio',
-                'answer' => 'nullable|required_unless:type,paragraph',
-            ]
-        );
-
-        if($validator->fails())
-        {
-            throw new ValidatorFailedException('Failed creating exam', $validator->errors());
-        }
-
-        $validated = $validator->validated();
-
+        $validated = $this->validateQuestion($data);
         return Question::create($validated);
-
     }
 
     public function deleteQuestion(int $id): void
     {
         Question::findOrFail($id)->delete();
+    }
+
+    private function validateQuestion(array $data)
+    {
+        $validator = Validator::make($data, 
+            [
+                'exam_id' => 'required', 
+                'type' => 'required|in:radio,single,paragraph',
+                'problem' => 'required',
+                'options' => 'nullable|array|prohibited_unless:type,radio|required_if:type,radio',
+                'answer' => 'nullable|prohibited_if:type,paragraph|required_unless:type,paragraph',
+            ], 
+        );
+
+        if($validator->fails())
+        {
+            $error_message = $validator->errors()->all();
+            throw new ValidatorFailedException($error_message[0], $validator->errors());
+        }
+
+        return $validator->validated();
     }
 
 }
