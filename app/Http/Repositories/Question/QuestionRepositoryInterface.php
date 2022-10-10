342c6195e079550b04a019f2a446bfd595bb880c
Donna Chelsey V. Dagcuta <donna@SYNTACTICS.LOCAL>
2022-10-10 09:15:33 +0800

    Setup custom message for Question entity

--- app/Http/Repositories/Question/QuestionRepositoryInterface.php
+++ app/Http/Repositories/Question/QuestionRepositoryInterface.php
@@ -9,9 +9,9 @@ use App\Models\Question;
 
 interface QuestionRepositoryInterface 
 {
-    public function createQuestion(array $data): Question;
-    public function editQuestion(array $data, int $id): Question;
-    public function deleteQuestion(int $id): void;
-    public function showAllQuestions(int $exam_id): Collection;
-    public function showSingleQuestion(int $id): Question;
+    public function createQuestion(array $data);
+    public function editQuestion(array $data, int $id);
+    public function deleteQuestion(int $id);
+    public function showAllQuestions(int $exam_id);
+    public function showSingleQuestion(int $id);
 }
