826938118bd59914264e2083a61dc914db5b32fe
Donna Chelsey V. Dagcuta <donna@SYNTACTICS.LOCAL>
2022-10-12 07:27:58 +0800

    Added prefix to Applicant routes

--- routes/api.php
+++ routes/api.php
@@ -37,9 +37,9 @@ Route::group(['middleware' => ['auth:sanctum']], function (){
 		Route::resource('position', PositionController::class);				
 	});
 
-	Route::group(['middleware' => ['role:applicant']], function() {
-		Route::get('/applicant/exams', [ExamController::class, 'getApplicantExams']);
-		Route::get('/applicant/exam/{id}', [ExamController::class, 'getSingleApplicantExam']);
+	Route::group(['prefix' => 'applicant', 'middleware' => ['role:applicant']], function() {
+		Route::get('/exams', [ExamController::class, 'getApplicantExams']);
+		Route::get('/exam/{id}', [ExamController::class, 'getSingleApplicantExam']);
 	});	
 	
 	// Route::group(['middleware' => ['role:applicant,admin']], function () {
