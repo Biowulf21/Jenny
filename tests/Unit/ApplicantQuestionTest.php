6483186e4172997e6813488347651d1ab40ace32
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2022-10-13 11:38:21 +0800

    Created unit test as base for onSubmitCheck function

--- tests/Unit/ApplicantQuestionTest.php
+++ tests/Unit/ApplicantQuestionTest.php
@@ -7,11 +7,13 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Collection;
 
 use App\Models\Position;
 use App\Models\User;
 use App\Models\Exam;
 use App\Models\Question;
+use App\Models\ApplicantQuestion;
 
 class ApplicantQuestionTest extends TestCase
 {
@@ -31,17 +33,36 @@ class ApplicantQuestionTest extends TestCase
      /**
      * @test
      */
-    public function sample_method()
+    public function case_one()
     {
+        $score = 0; 
+        $questionsArray = [];
+
         $position = Position::factory()->create(['name' => 'Position']);
         $user = User::factory()->create(['role' => 'applicant', 'for_position' => $position->id]);
         $exam = Exam::factory()->create(['for_position' => $position->id]);
-        $questions = Question::factory(5)->create(['exam_id' => $exam->id]);
+        $questions = Question::factory(5)->create(['exam_id' => $exam->id, 'type' => $this->faker->randomElement(['radio', 'single'])]);
+        
+        foreach ($questions as $question) {
+            ApplicantQuestion::factory()->create(['applicant_id' => $user->id, 'question_id' => $question->id, 'answer' => $question->answer]);
+        }
 
-        Log::info($position); 
-        Log::info($user);
-        Log::info($exam);
-        Log::info($questions);
-
+        // checking 
+        foreach ($questions as $question) {
+            $answer = ApplicantQuestion::where([ ['applicant_id', $user->id], ['question_id', $question->id] ])->first();
+            
+            if ($question->type !== 'paragraph') 
+            {
+                if ($answer->answer === $question->answer) 
+                {
+                    $score++; 
+                    $answer->isCorrect = true;
+                }
+                
+                $answer->isChecked = true;
+                $answer->save(); 
+                log::info($answer);
+            }
+        }
     }
 }
